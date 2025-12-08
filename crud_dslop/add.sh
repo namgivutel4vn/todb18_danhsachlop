1f78ad2985d0be845ee702d2e7938cf6bfe031bd
Nam <namgivu@gmail.com>
2025-12-08 15:51:50 +0000

    fix sed -i''

--- crud_dslop/add.sh
+++ crud_dslop/add.sh
@@ -1,8 +1,8 @@
 # xoá dòng </pre> truoc khi them
-sed -i '' '/<\/pre>/d' ./dslop.html
+sed -i'' '/<\/pre>/d' ./dslop.html
 #                  /d de xoa cho enh sed
 #   -i inplace la sed vao tep tructiep taicho ; ko co -i se in ketqua ra
-#   -i '' de chay dc tren macbook va linux
+#   -i'' de chay dc tren macbook va linux
 
 echo $1 >> ./dslop.html
 
