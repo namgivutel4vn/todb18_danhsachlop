97f3b3841957871ce0ed6d42e92a0417992a860e
Nam G. VU <namgivu@gmail.com>
2025-12-19 22:21:21 +0700

    on linux ve ../ khi chay tep sh

---
+++ linux-learn/s0_dotdotdir/_.sh
@@ -0,0 +1,18 @@
+pwd
+cd ..
+pwd
+
+_=cat<<EOT
+nn@macbookm1 /tmp
+$ /Users/nn/Desktop/_/todb18_danhsachlop/linux-learn/s0_dotdotdir/_.sh
+/tmp
+/
+
+---
+
+nn@macbookm1 /Users/nn
+$ /Users/nn/Desktop/_/todb18_danhsachlop/linux-learn/s0_dotdotdir/_.sh
+/Users/nn
+/Users
+
+EOT
