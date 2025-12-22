ec5175a77ab9043e4e66c7bb11b3407549434969
Nam G. VU <namgivu@gmail.com>
2025-12-22 20:16:36 +0700

    review

--- docker-learn/mynginxwenv/run.sh
+++ docker-learn/mynginxwenv/run.sh
@@ -1,8 +1,8 @@
 set -e
     SH=`cd $(dirname $0) && pwd`
 cd $SH
-docker build                  -t mynginxwenv-i .  --no-cache  --build-arg name_a_build_arg="$1"
-docker run -e NAME="$1" -p880:80 mynginxwenv-i
+docker build     -t mynginxwenv-i .  --no-cache  --build-arg name_a_build_arg="$1"
+docker run -p880:80 mynginxwenv-i
 
 _='
 --- whatsnext cmd
