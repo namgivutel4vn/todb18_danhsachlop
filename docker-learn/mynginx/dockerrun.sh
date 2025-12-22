0218911bf9b046bda3fc699677e5a0f0decb78d5
Tung Doan <doansontung@yahoo.com>
2025-12-22 22:54:35 +0700

    Add HTML

--- docker-learn/mynginx/dockerrun.sh
+++ docker-learn/mynginx/dockerrun.sh
@@ -3,4 +3,10 @@ cd $SH
 
 cp ./index.html.origin ./v/index.html
 
+NAME=${1:-${n}}
+
+sed -i '' "s/\${NAME}/$NAME/g" ./v/index.html
+
+
+
 docker compose -p tung up -d --force-recreate --remove-orphans
