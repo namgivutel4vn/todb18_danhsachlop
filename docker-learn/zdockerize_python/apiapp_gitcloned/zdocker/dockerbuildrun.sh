5cc377fbbe6d5249c335b1846e8d6226134e3327
Nam G. VU <namgivu@gmail.com>
2025-12-26 23:01:10 +0700

    run apiapp_c

--- docker-learn/zdockerize_python/apiapp_gitcloned/zdocker/dockerbuildrun.sh
+++ docker-learn/zdockerize_python/apiapp_gitcloned/zdocker/dockerbuildrun.sh
@@ -1,7 +1,16 @@
 SH=$(cd `dirname $0` && pwd)
 
+docker rm -f apiapp-c
+
 set -e  # halt if err
 
 #      build context                -f      Dockerfile   -t image
 docker build "$SH/../flask_apiapp"  -f "$SH/Dockerfile"  -t flask-apiapp-i  --progress plain
-docker run                                                  flask-apiapp-i
+docker run -p22333:5000 -d --name apiapp-c                  flask-apiapp-i
+
+echo
+cat <<EOT
+--- whatsnext cmd
+curl -sSLf localhost:22333
+{}
+EOT
