a3b306380d0a4298b1ae853a9660f88308407715
Nam G. VU <namgivu@gmail.com>
2025-12-29 20:25:22 +0700

    Dockerfile_samedir_w_src

---
+++ docker-learn/zdockerize_python/apiapp_gitcloned2__Dockerfile_samedir_w_src/flask_apiapp/composebuildup.sh
@@ -0,0 +1,22 @@
+SH=$(cd `dirname $0` && pwd)
+
+cd $SH
+docker compose --progress=plain build #--no-cache
+
+echo
+docker compose --progress=plain up --force-recreate -d --remove-orphans
+#      compose                  up --force-recreate to always create container ie reload envvar ifany
+
+echo
+cat <<EOT
+--- whatsnext cmd
+docker logs flask_apiapp_c
+docker logs flask_apiapp_c -f
+
+cd $SH
+docker compose logs
+docker compose logs -f
+
+curl -sSLf localhost:22333
+{}
+EOT
