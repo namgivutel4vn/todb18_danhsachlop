540e9f57e5bfad114e5d62130f38a5a05381ca06
Tung Doan <doansontung@yahoo.com>
2026-01-05 00:05:28 +0700

    Add dockerize practice

---
+++ docker-learn/tung_practice/dockerize_python/apiapp_gitcloned3__w_prefix/flask_apiapp/composebuildup_w_prefix.sh
@@ -0,0 +1,13 @@
+SH=$(cd `dirname $0` && pwd)
+cd $SH
+
+docker compose build
+
+echo
+docker compose -p myprefix up --force-recreate -d --remove-orphans
+
+echo
+cat <<EOT
+curl -sSLf localhost:33300
+{}
+EOT
