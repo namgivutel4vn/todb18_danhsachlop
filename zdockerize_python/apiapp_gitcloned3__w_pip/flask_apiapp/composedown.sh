52b903a69b48d49b2f4562bfffce429ef568c2e1
phunp <phongphufpt2009@gmail.com>
2026-01-16 13:17:50 +0700

    Delete zdockerize_python directory

--- zdockerize_python/apiapp_gitcloned3__w_pip/flask_apiapp/composedown.sh
+++
@@ -1,4 +0,0 @@
-SH=$(cd `dirname $0` && pwd)
-
-cd $SH
-docker compose --progress=plain down --remove-orphans
--- zdockerize_python/webapp_gitcloned/flask_webapp/zdocker-image-fr-dockerhub/zcomposebuildup--image-fr-dockerhub.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-SH=$(cd $(dirname $0); pwd)
-
-docker compose \
-  -f "$SH/docker-compose--image-fr-dockerhub.yml" \
-  --progress=plain up --force-recreate -d  --remove-orphans
