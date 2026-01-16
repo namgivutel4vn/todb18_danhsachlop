7ff177dd74b2d9a05d8b88f8243edd624d126cbd
phunp <phongphufpt2009@gmail.com>
2026-01-16 13:14:57 +0700

    Delete docker-compose directory

--- docker-compose/mynginx_s_CMD/s1_nginx_hiname_envvar/composedown.sh
+++
@@ -1,4 +0,0 @@
-SH=`cd $(dirname $0) && pwd`
-cd $SH
-
-docker compose -p todb18 down --remove-orphans --volumes
--- docker-compose/mynginx_svol/composeup.sh
+++
@@ -1,4 +0,0 @@
-SH=`cd $(dirname $0) && pwd`
-cd $SH
-
-docker compose -p todb18 up -d --force-recreate
