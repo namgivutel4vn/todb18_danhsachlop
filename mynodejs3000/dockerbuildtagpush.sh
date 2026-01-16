c33a06af59449bb9ee338147e3e7d3cdde449842
phunp <phongphufpt2009@gmail.com>
2026-01-16 13:16:35 +0700

    Delete mynodejs3000 directory

--- mynodejs3000/dockerbuildtagpush.sh
+++
@@ -1,9 +0,0 @@
-     SH='cd $(dirname $0) && pwd' 
-cd $SH
-
-docker build . -t mynodejs3000 --platform linux/amd64,linux/arm64
-
-docker tag 	  mynodejs3000 locphu2020/mynodejs3000
-#      tag        tenfile  tencuaban/xx
-
-docker push               locphu2020/mynodejs3000
