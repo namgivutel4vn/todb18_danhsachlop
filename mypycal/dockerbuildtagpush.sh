c929c95a7729a73c68a160c4e66db17620e0cd71
phunp <phongphufpt2009@gmail.com>
2026-01-16 13:16:45 +0700

    Delete mypycal directory

--- mypycal/dockerbuildtagpush.sh
+++
@@ -1,9 +0,0 @@
-     SH='cd $(dirname $0) && pwd' 
-cd $SH
-
-docker build . -t mypycal --platform linux/amd64,linux/arm64
-
-docker tag 	  mypycal locphu2020/mypycal
-#      tag        tenfile  tencuaban/xx
-
-docker push               locphu2020/mypycal
